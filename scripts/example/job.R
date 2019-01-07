fc9842b871c8be1cb6886d182d3972d77c215c9f
Dennis Stumm <dstumm@yandex.ru>
2019-01-07 11:56:10 +0100

    Add documentation to example

--- scripts/example/job.R
+++ scripts/example/job.R
@@ -1,13 +1,37 @@
-# TODO: Add doc
+#' This file is part of R-cluster.
+#'
+#' R-cluster is free software: you can redistribute it and/or modify
+#' it under the terms of the GNU General Public License as published by
+#' the Free Software Foundation, either version 3 of the License, or
+#' (at your option) any later version.
+#'
+#' R-cluster is distributed in the hope that it will be useful,
+#' but WITHOUT ANY WARRANTY; without even the implied warranty of
+#' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#' GNU General Public License for more details.
+#'
+#' You should have received a copy of the GNU General Public License
+#' along with R-cluster.  If not, see <http://www.gnu.org/licenses/>.
+#'
+#' This file contains a sample job which is used to analyze files.
 
+#' Analyzes the file passed to this function.
+#'
+#' @param filePath The path to the file, that should be analyzed.
+#'
+#' @return A string containing csv formatted stats for the passed file.
+#' @export
 run <- function(filePath) {
+  # Read all lines of the file. 
   lines <- readLines(filePath, warn=FALSE)
 
+  # Iterate over the lines and anlyze them.
   result <- foreach (i=lines, .combine="combineResults") %dopar% {
     partitialResult <- c()
     characters <- unlist(strsplit(i, ""))
     partitialResult["Gesamt"] <- length(characters)
 
+    # Iterate over each single character and adjust the stats.
     foreach (character=characters) %do% {
       if (character == " ") {
         character = "Leerzeichen"
@@ -31,6 +55,12 @@ run <- function(filePath) {
   return(capture.output(write.csv(result)))
 }
 
+#' Merges the stats of two subresults.
+#'
+#' @param map1 Vector with the first subresult.
+#' @param map2 Vector with the second subresult.
+#'
+#' @return A vector containing the merged stats of the subresults.
 combineResults <- function(map1, map2) {
   result <- map1
 
